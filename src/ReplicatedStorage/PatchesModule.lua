f544434867536348ab8b1eedbc067a58c629825f
Aaron Duke <122709961+deepBluePioneer@users.noreply.github.com>
2024-06-01 04:02:53 -0400

    ups

--- src/ReplicatedStorage/PatchesModule.lua
+++ src/ReplicatedStorage/PatchesModule.lua
@@ -1,6 +1,6 @@
 --- PatchesModule
 -- This module contains definitions for various patches that modify machine stats.
--- @module PatchesModules
+-- @module PatchesModule
 
 --- Table holding all patch definitions.
 -- @field TurnPatch Patch that modifies the Turn stat.
