964994614338f8445c07954c728304c519c3bc84
Aaron Duke <122709961+deepBluePioneer@users.noreply.github.com>
2025-07-08 23:30:13 -0400

    Iris Set up

--- src/ServerScriptService/KnitServerRuntime.server.lua
+++ src/ServerScriptService/KnitServerRuntime.server.lua
@@ -7,12 +7,10 @@ local Knit = require(ReplicatedStorage.Packages.Knit)
 local ServerServices = ServerStorage.Source.ServerServices
 
 -- Define the Place IDs for different game environments
-local DungeonPlaceID = 17399051565
-local HubWorldServiceID = 17399041158
+local HubWorldServiceID = 128423752054643
 
 -- Define service directories based on game type
 local HubWorldServices = ServerServices.HubWorldServices
-local DungeonPlaceServices = ServerServices.DungeonPlaceServices
 
 -- Function to require services recursively
 local function requireServices(directory)
@@ -30,11 +28,6 @@ local function loadServicesForPlace(placeId)
     local serviceDirectory
     if placeId == HubWorldServiceID then
         serviceDirectory = HubWorldServices
-    elseif placeId == DungeonPlaceID then
-        serviceDirectory = DungeonPlaceServices
-    else
-        warn("Unrecognized Place ID, no services loaded")
-        return
     end
 
     requireServices(serviceDirectory)
--- src/StarterPlayerScripts/KnitClientRuntime.client.lua
+++ src/StarterPlayerScripts/KnitClientRuntime.client.lua
@@ -4,14 +4,12 @@ local StarterPlayerScripts = StarterPlayer.StarterPlayerScripts
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 -- Define the Place IDs for different game environments
-local DungeonPlaceID = 17399051565
-local HubWorldServiceID = 17399041158
+local HubWorldServiceID = 128423752054643
 
 -- References to controller directories
 local ClientControllers = StarterPlayerScripts.Source.ClientControllers
 local HubWorldControllers = ClientControllers.HubWorldControllers
-local DungeonControllers = ClientControllers.DungeonControllers.ProcGenControllers_v3
-local ProcGenControllers = DungeonControllers
+
 
 -- Function to require controllers recursively
 local function requireControllers(directory)
@@ -29,13 +27,6 @@ local function loadControllersForPlace(placeId)
     local controllerDirectory
     if placeId == HubWorldServiceID then
         controllerDirectory = HubWorldControllers
-        print(controllerDirectory)
-    elseif placeId == DungeonPlaceID then
-        controllerDirectory = ProcGenControllers
-        print(controllerDirectory)
-    else
-        warn("Unrecognized Place ID, no controllers loaded")
-        return
     end
 
     requireControllers(controllerDirectory)
